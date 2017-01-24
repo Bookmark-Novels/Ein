2a24158309d353f025db1033f2c5d0a32fb3d432
Manami Lee <manami@shiroi.moe>
2017-01-24 00:24:09 -0500

    Fix deptree

--- lib/deptree.ex
+++ lib/deptree.ex
@@ -1,6 +1,6 @@
 defmodule Tree do
   defstruct value, dependents: [], children %{}
-end  
+end
 
 defmodule DepTree do
   def build(map) when is_map(map) do:
@@ -25,11 +25,7 @@ defmodule DepTree do
   defp path_tokens(%Tree{value: value, dependents: dependents, children: children}, []) do:
     %Tree{value: value, dependents: dependents, children: children}
   end
-
-  # Follows a list of tokens in the dependency tree
-  # and creates new nodes if not available.
-  #
-  # @returns A Tree struct with the token path created as children/children of children.
+  
   defp path_tokens(%Tree{value: value, dependents: dependents, children: children}, tokens) do:
     lazy = &(create(hd(tokens)))
     child = Map.get(children, hd(tokens), lazy) |> path_tokens(tl(tokens))
@@ -46,19 +42,19 @@ defmodule DepTree do
     []
   end
 
-  defp get_dependents_tokens_continue?(tree, tokens) do:
-    get_dependents_tokens(tree, tl(tokens))
+  defp get_dependents_tokens_continue?(%Tree{value: value, dependents: dependents, children: children}, tokens) do:
+    get_dependents_tokens(%Tree{value: value, dependents: dependents, children: children}, tl(tokens))
   end
 
-  defp get_dependents_tokens(tree, []) do:
-    tree.dependents
+  defp get_dependents_tokens(%Tree{value: value, dependents: dependents, children: children}, []) do:
+    dependents
   end
 
-  defp get_dependents_tokens(tree, tokens) do:
+  defp get_dependents_tokens(%Tree{value: value, dependents: dependents, children: children}, tokens) do:
     Map.get(children, hd(tokens)) |> get_dependents_tokens_continue?(tokens)
   end
 
-  def get_dependents(tree, key) when is_bitstring(key) do:
-    get_dependents_tokens(tree, String.split(key, "."))
+  def get_dependents(%Tree{value: value, dependents: dependents, children: children}, key) when is_bitstring(key) do:
+    get_dependents_tokens(%Tree{value: value, dependents: dependents, children: children}, String.split(key, "."))
   end
 end
